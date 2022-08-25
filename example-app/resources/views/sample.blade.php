6fc3a6ecaac92df295b0c7686211038d2e581ce9
r-shirasu <risako.shirasu@asia-quest.jp>
2022-08-25 20:47:21 +0900

    途中まで

---
+++ example-app/resources/views/sample.blade.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="d-flex">
+    <div class="flex-column bg-light border-end border-primary" style="width: 280px; height: 1000px;">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+            <span class="fs-4">サイドバー</span>
+        </a>
+        <ul>
+            <li>リンク</li>
+            <li>リンク</li>
+            <li>リンク</li>
+            <li>リンク</li>
+            <li>リンク</li>
+            <li>リンク</li>
+        </ul>
+    </div>
+    <div style="width: 100%;">
+        <div>ヘッダー部分</div>
+        <table class="table">
+            <thead>
+            <tr>
+                <th>check</th>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+
+                <th scope="row">check</th>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">check</th>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">check</th>
+                <td>3</td>
+                <td colspan="2">Larry</td>
+                <td>@twitter</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+
+</body>
+</html>
+
+
